617061ed2601e2646890332a70ae6b300fda0d82
skreadz <dylan.ortega1903@gmail.com>
2023-09-01 14:56:56 +0200

    exam

---
+++ powershell/ad/ad_users_exam.ps1
@@ -0,0 +1,142 @@
+### VARIABLES ###
+$cheminbase = (Get-Addomain).distinguishedname
+$ou_principale = Read-Host "Saisir OU principale"
+$serveur = Read-host "Saisir le nom DNS du premier serveur de fichier"
+$cheminouprincipale = "OU=$ou_principale,$cheminbase"
+$cheminuser = "OU=SERVICES,"+$cheminouprincipale
+$chemingroupe = "OU=GROUPES,"+$cheminouprincipale
+$cheminservices = "\\"+$serveur+"\partage\SERVICES\"
+$cheminperso = "\\"+$serveur+"\partage\PERSO\"
+$ou_default = @("GROUPES","SERVICES","ORDINATEURS")
+$csv = Import-Csv -Path "C:\Users\Administrateur\Documents\user.csv" -Delimiter ";" -Encoding UTF8
+### VARIABLES ###
+
+function pause ($message="Appuyer sur touche pour continuer") 
+{
+    Write-Host -NoNewline $message
+    $null = $Host.UI.RawUI.ReadKey("noecho,includeKeydown")    #### Fonction Pause
+    Write-Host ""    
+}
+
+function menu () #### Fonction Menu
+{
+    Clear-Host
+    Write-Host "Menu Script"
+
+    Write-Host "1 : Création OU principale"
+    Write-Host "2 : Création groupe"
+    Write-Host "3 : Création dossier partages"
+    Write-Host "4 : Création des utilisateurs"
+    Write-Host "5 : ACL dossier PERSO"
+    Write-Host "6 : ACL dossier SERVICES"
+    Write-Host "Q : Quitter"
+    $choix = Read-Host "votre choix "
+    switch ($choix) {
+        1 {ou_principale;pause;menu}
+        2 {groupe;pause;menu}
+        3 {dossier;pause;menu}
+        4 {user;pause;menu}
+        5 {acl_user;pause;menu}
+        6 {acl_services;pause;menu}
+        Q {exit}
+        Default {menu}   
+}
+}
+
+function ou_principale () {         #### Fonction pour créer OU principale ex: SQLI + IMPRIMANTES,GROUPES,UTILISATEURS,ORDINATEURS,SERVEURS
+
+    New-ADOrganizationalUnit -Name $ou_principale -Path $cheminbase
+    
+    ForEach ($line in $ou_default) {
+    New-ADOrganizationalUnit -Name $line -Path $cheminouprincipale -Verbose -ProtectedFromAccidentalDeletion:$false
+    }
+
+    ForEach ($line in $csv) {
+    $ou_user = $line.ou.toUpper()
+    New-ADOrganizationalUnit -Name $ou_user -Path $cheminuser -Verbose -ProtectedFromAccidentalDeletion:$false     
+    }
+}
+function groupe () {        #### Fonction pour créer les groupes GG,GU,GDL en fonction des services du CSV
+
+    ForEach ($line in $csv) {
+        $group = $line.group.toUpper()      #### Récupère la section group du CSV en plus de l'écrire en majuscules
+        $GG="GG_"+$group
+        $GU="GU_"+$group
+        $GDL="GDL_"+$group+"_RW"
+ 
+        New-ADGroup -GroupCategory Security -GroupScope Global -Name $GG -Path $chemingroupe -Verbose
+                
+        New-ADGroup -GroupCategory Security -GroupScope Global -Name $GU -Path $chemingroupe -Verbose
+                 
+        New-ADGroup -GroupCategory Security -GroupScope Global -Name $GDL -Path $chemingroupe -Verbose
+
+        Add-ADGroupMember -Identity $GU -Members  $GG -Verbose
+        Add-ADGroupMember -Identity $GDL -Members  $GU -Verbose       
+    }
+}
+
+function dossier () {                                               #### Fonction pour créer les dossiers perso users + dossier de services sur le serveur \\SRV-SF1\partage
+    
+    ForEach ($line in $csv) {
+        $group = $line.ou.toUpper()
+        $sam = $line.prenom.toLower()+"."+$line.nom.toLower()
+        #$chemintest = $cheminservices+$group
+        #$existe = Test-Path -Path $chemintest
+            #if (!$existe) {                                             #### Test si le chemin existe sinon il ne lance pas la commande
+        New-Item -ItemType Directory -Name $group -Path $cheminservices -Verbose
+            #}
+        New-Item -ItemType Directory -Name $sam -Path $cheminperso -Verbose
+    }
+}
+
+function user {         #### Fonction pour créer utilisateur
+
+    ForEach ($line in $csv) {
+        $sam = $line.prenom.toLower()+"."+$line.nom.toLower()
+        $upn = $sam+"@"+$env:USERDNSDOMAIN.toLower()
+        $mail = $upn
+        $group = $line.ou.toUpper()
+        $cheminusergroup = "OU="+$group+",OU=UTILISATEURS,"+$cheminouprincipale
+        $prenom = $line.prenom.toLower()
+        $nom = $line.nom.toLower()
+
+        New-ADUser -Name $nom -GivenName $prenom -Surname $nom -SamAccountName $sam -UserPrincipalName $upn -EmailAddress $mail -Enable $true -ChangePasswordAtLogon $true -Path $cheminusergroup -Verbose -AccountPassword (convertto-securestring "Azerty1+" -asplaintext -force)
+    }
+}
+
+function acl_user {
+
+    ForEach ($line in $csv) {
+        $sam = $line.prenom.toLower()+"."+$line.nom.toLower()
+        $service = "\\"+$serveur+"\partage\PERSO\"+$sam
+        $group = $line.group.toUpper()      #### Récupère la section group du CSV en plus de l'écrire en majuscules
+        $GG="GG_"+$group
+
+        Add-ADGroupMember -Identity $GG -Members $sam
+
+        $acl = Get-Acl -Path $service
+        $rule = New-Object Security.AccessControl.FileSystemAccessRule("$sam", "Modify" ,"ContainerInherit, ObjectInherit","None","Allow")
+        $acl.SetAccessRuleProtection($true,$true)
+        $acl.addAccessRule($rule) #ajout de la régle
+
+        $acl | Set-Acl #Appliquer les droits
+        }
+}
+
+function acl_services {
+    
+    ForEach ($line in $csv) {
+        $ou = $line.ou.toUpper() 
+        $service = "\\"+$serveur+"\partage\SERVICES\"+$ou
+        $group = $line.group.toUpper()                  #### Récupère la section group du CSV en plus de l'écrire en majuscules
+        $GDL="GDL_"+$group+"_RW"
+
+        $acl = Get-Acl -Path $service
+        $rule = New-Object Security.AccessControl.FileSystemAccessRule("$GDL", "Modify" ,"ContainerInherit, ObjectInherit","None","Allow")
+        $acl.SetAccessRuleProtection($true,$true)
+        $acl.addAccessRule($rule)           #ajout de la régle
+
+        $acl | Set-Acl      #Appliquer les droits
+    }
+}
+menu
